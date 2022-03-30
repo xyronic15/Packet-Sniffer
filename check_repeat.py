0520896284bc9583bf12ba6d62168b8112507979
Xyron Brual <xyronb@gmail.com>
2022-03-30 15:04:45 -0400

    Created sniffer and blocker objects
    - Need to try it on a single driver

---
+++ check_repeat.py
@@ -0,0 +1,8 @@
+with open('p_threats.txt') as f:
+    seen = set()
+    for line in f:
+        line_lower = line.lower()
+        if line_lower in seen:
+            print(line)
+        else:
+            seen.add(line_lower)
