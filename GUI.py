3a9b42d49b5a211564108eceb8cb32b48e72bc4f
Janahan10 <janahan.ravichandran@gmail.com>
2022-04-01 15:33:48 -0400

    added gui file

---
+++ GUI.py
@@ -0,0 +1,34 @@
+import PySimpleGUI as sg
+
+# dummy list to simulate list of sniffed packets !--REMOVE WHEN INTEGRATING--!
+sniffed = ['Ether / IPv6 / UDP / DNS Qry "b''discord.com.''"',
+           'Ether / IPv6 / UDP / DNS Qry "b''discord.com.''"',
+           'Ether / IPv6 / UDP / DNS Qry "b''discord.com.''"',
+           'Ether / IPv6 / UDP / DNS Qry "b''discord.com.''"',
+           'Ether / IPv6 / UDP / DNS Qry "b''discord.com.''"']
+
+
+# dummy list to simulate list of blocked ip !--REMOVE WHEN INTEGRATING--!
+ips = ['10.18.254.151', '209.165.226.40', '86.128.213.12', '209.165.226.40', '41.130.248.117']
+
+# create a string with newline delimiter for printing
+list_packets = '\n'.join([str(i) for i in ips])
+list_sniffed = '\n'.join([str(i) for i in sniffed])
+
+# layout of gui window
+layout = [[sg.Text('Summary of Packets Sniffed:', font=20)],
+          [sg.Multiline(list_sniffed, size=(75,10), disabled=True, key='key-multiline')],
+          [sg.Multiline(list_packets, size=(75,10), disabled=True, key='key-multiline')]
+          ]
+
+# create the window
+window = sg.Window('Window Title', layout)
+
+# loop through to keep window open till its closed
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
+
+window.close()
